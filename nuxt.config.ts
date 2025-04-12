208b511f97ab417f5d105c94ffc0ff512436fcc9
Nathan <nath2754@gmail.com>
2025-04-12 13:14:33 +0200

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/test-utils',
+    '@nuxt/scripts',
+    '@nuxt/fonts'
+  ]
+})
